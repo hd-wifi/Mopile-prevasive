677e28ba7a0c2925415a3d86475442410e1f81bf
hd-wifi <hxydyfih@gmail.com>
2025-01-19 13:00:41 +0300

    Add files via upload

---
+++ moohmed/bin/1.dart
@@ -0,0 +1,9 @@
+void main() {
+  String text = "Teenager";
+  int age = 22;
+  if (age >= 13 && age <= 19) {
+    print(text);
+  } else {
+    print("not $text");
+  }
+}
---
+++ moohmed/bin/moohmed.dart
@@ -0,0 +1,6 @@
+import 'package:moohmed/moohmed.dart' as moohmed;
+
+void main(List<String> arguments) {
+  print('Hello world: ${moohmed.calculate()}!');
+}
+
